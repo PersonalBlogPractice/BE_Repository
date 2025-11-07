37029f8bde7d60a4f691fd43d355484b50117a4b
tato126 <heechan.log@gmail.com>
2025-11-07 16:21:30 +0900

    phase1 Post 엔티티

--- src/main/java/io/github/tato126/practice/post/entity/Post.java
+++ src/main/java/io/github/tato126/practice/post/entity/Post.java
@@ -1,12 +1,16 @@
 package io.github.tato126.practice.post.entity;
 
+import io.github.tato126.practice.post.dto.request.PostRequest;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
+@Getter
 @NoArgsConstructor
 @Entity
 public class Post {
@@ -19,8 +23,10 @@ public class Post {
 
     private String content;
 
+    private String authorName;
+
     @Enumerated(EnumType.STRING)
-    private PostStatus postStatus = PostStatus.DOING;
+    private PostStatus postStatus = PostStatus.DRAFT;
 
     @CreatedDate
     private LocalDateTime createdAt;
@@ -29,7 +35,23 @@ public class Post {
     private LocalDateTime updatedAt;
 
     public enum PostStatus {
-        DOING,
-        DONE
+        DRAFT,
+        PUBLISHED
+    }
+
+    @Builder
+    public Post(String title, String content, PostStatus postStatus) {
+        this.title = title;
+        this.content = content;
+        this.postStatus = postStatus;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public static Post form(PostRequest request) {
+        return Post.builder()
+                .title(request.title())
+                .content(request.content())
+                .build();
     }
 }
