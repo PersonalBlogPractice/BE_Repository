4b0fa94ae6ad2448da6d64f6109e33c98be262c7
tato126 <heechan.log@gmail.com>
2025-11-07 20:05:39 +0900

    swagger 설정

--- src/main/java/io/github/tato126/practice/config/SwaggerConfig.java
+++ src/main/java/io/github/tato126/practice/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package io.github.tato126.practice.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,8 +19,16 @@ public class SwaggerConfig {
 
     private Info info() {
         return new Info()
-                .title("Demo Board")
-                .description("Demo Board")
-                .version("1.0");
+                .title("DevBlog API")
+                .description("개발자 블로그 플랫폼 RESTful API")
+                .version("1.0.0");
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
     }
 }
