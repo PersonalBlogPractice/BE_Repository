e2eae72b90a9354f7e97ea450b52ac7a47cd9a34
tato126 <heechan.log@gmail.com>
2025-11-10 15:47:57 +0900

    비밀번호 불일치 예외 클래스 수정

--- src/main/java/io/github/tato126/practice/common/excetion/login/InvalidPasswordException.java
+++ src/main/java/io/github/tato126/practice/common/excetion/login/InvalidPasswordException.java
@@ -2,11 +2,7 @@ package io.github.tato126.practice.common.excetion.login;
 
 public class InvalidPasswordException extends RuntimeException {
 
-    public InvalidPasswordException(String email) {
-        super("사용자를 찾을 수 없습니다: " + email);
-    }
-
     public InvalidPasswordException() {
-        super("사용자를 찾을 수 없습니다.");
+        super("비밀번호가 일치하지 않습니다.");
     }
 }
