6478ab0e5048b60f736853b39db7ff6f17058b3a
tato126 <heechan.log@gmail.com>
2025-11-11 14:29:37 +0900

    swagger에 토큰 추가 및 검증

--- src/main/java/io/github/tato126/practice/config/SwaggerConfig.java
+++ src/main/java/io/github/tato126/practice/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package io.github.tato126.practice.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +12,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, securityScheme()))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .info(info());
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .name(SECURITY_SCHEME_NAME)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .description("JWT 토큰을 입력하세요 (Bearer 제외)");
     }
 
     private Info info() {
